5710e8f5a3da404eb3823aa10b8a3b9e453c5322
saifty <ahmedsaif.developer@gmail.com>
2018-06-12 16:04:00 +0800

    Handles mongodb connections and (populates/unpopulates data)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ import routes from './routes';
 import anxpro from './anxpro';
 import bitfinex from './bitfinex';
 
+//=====> Mongo Client Addons - to be put into seperate module
+var MongoClient = require('mongodb').MongoClient;
+var config = require('../_config');
+const mongoUrl = config.mongoURI['test'];
+//<====== Mongo client addons - to be put into seperate module
+
+let client;
+
 const server = Hapi.server({
    port: 3000,
    host: 'localhost'
@@ -12,24 +20,45 @@ const server = Hapi.server({
 
 server.route(routes);
 
+//========> Mongo Connection handlers
+const initMongoConnection = async () => {
+    client = await MongoClient.connect(mongoUrl);
+    console.log("Database connection established");
+}
+const closeMongoConnection = () => {
+    if(client) {
+        client.close();
+        console.log("Connection to database closed");
+    }
+}
+//<======== Mongo Connection Handlers
+
+
 const init = async () => {
-   await server.register([
-      {
-         plugin: require('hapi-pino'),
-         options: {
+    await initMongoConnection();
+    await server.register([
+        {
+            plugin: require('hapi-pino'),
+            options: {
             prettyPrint: true,
             logEvents: false
-         }
-      },
-      bitfinex
-   ]);
+            }
+        },
+        {
+            plugin: bitfinex,
+            options: {
+                dbClient: client
+            }
+        }
+    ]);
 
-   await server.start();
-   console.log(`Server running at: ${server.info.uri}`);
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
    console.log(err);
+   closeMongoConnection();
    process.exit(1);
 });
 
